ba28bbcb25627c8f60c38fe8d7fac037a9b26124
Ian Braga <145636380+IanBraga96@users.noreply.github.com>
2024-06-04 14:17:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'senha-secreta-rs'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
