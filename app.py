f2bc602bc454f14041d42eda8c9efe2c9e4db4bd
Ian Braga <145636380+IanBraga96@users.noreply.github.com>
2024-06-04 14:17:18 -0300

    Update app.py
    Problema de usuario - banco de dados RESOLVIDA

--- app.py
+++ app.py
@@ -27,12 +27,14 @@ class Wallet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     total_value = db.Column(db.Float, nullable=False)
     total_expenses = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     expense_type_id = db.Column(db.Integer, db.ForeignKey('expense_type.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -40,16 +42,19 @@ class Revenue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     revenue_type_id = db.Column(db.Integer, db.ForeignKey('revenue_type.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class ExpenseType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class RevenueType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 #Define várias rotas para o Flask
@@ -118,84 +123,109 @@ def dashboard():
         flash('Por favor, faça login para acessar essa página.', 'warning')
         return redirect(url_for('login'))
     
-    wallet = Wallet.query.first()
-    expense_types = ExpenseType.query.all()
-    revenue_types = RevenueType.query.all()
-    total_revenue = db.session.query(db.func.sum(Revenue.amount)).scalar() or 0
-    total_expense = db.session.query(db.func.sum(Expense.amount)).scalar() or 0
+    user_id = session['user_id']
+    wallet = Wallet.query.filter_by(user_id=user_id).first()
+    expense_types = ExpenseType.query.filter_by(user_id=user_id).all()
+    revenue_types = RevenueType.query.filter_by(user_id=user_id).all()
+
+    total_revenue = db.session.query(db.func.sum(Revenue.amount)).filter(Revenue.user_id == user_id).scalar() or 0
+    total_expense = db.session.query(db.func.sum(Expense.amount)).filter(Expense.user_id == user_id).scalar() or 0
     total_value = total_revenue - total_expense
     
     # Calcular dados para o gráfico
-    expense_types = ExpenseType.query.all()
-    revenue_types = RevenueType.query.all()
+    expense_types = ExpenseType.query.filter_by(user_id=user_id).all()
+    revenue_types = RevenueType.query.filter_by(user_id=user_id).all()
     
     expense_data = {expense_type.name: 0 for expense_type in expense_types}
     revenue_data = {revenue_type.name: 0 for revenue_type in revenue_types}
     
-    for expense in Expense.query.all():
+    for expense in Expense.query.filter_by(user_id=user_id).all():
         expense_type = ExpenseType.query.get(expense.expense_type_id)
         expense_data[expense_type.name] += expense.amount
         
-    for revenue in Revenue.query.all():
+    for revenue in Revenue.query.filter_by(user_id=user_id).all():
         revenue_type = RevenueType.query.get(revenue.revenue_type_id)
         revenue_data[revenue_type.name] += revenue.amount
 
     labels = list(expense_data.keys()) + list(revenue_data.keys())
     data = list(expense_data.values()) + list(revenue_data.values())
 
-    expenses = Expense.query.all()
-    revenues = Revenue.query.all()
+    expenses = Expense.query.filter_by(user_id=user_id).all()
+    revenues = Revenue.query.filter_by(user_id=user_id).all()
     
     return render_template('dashboard.html', wallet=wallet, total_value=total_value,
                            total_expense=total_expense, labels=labels, data=data, expense_types=expense_types,
-                           revenue_types=revenue_types, expenses=expenses, revenues=revenues)
+                           revenue_types=revenue_types, expenses=expenses, revenues=revenues, user_id=user_id)
 
 @app.route('/add_wallet', methods=['POST'])
 def add_wallet():
     total_value = request.form.get('total_value')
     total_expenses = request.form.get('total_expenses')
-    wallet = Wallet.query.first()
+    user_id = session['user_id']
+    wallet = Wallet.query.filter_by(user_id=user_id).first()
     if wallet:
         wallet.total_value = total_value
         wallet.total_expenses = total_expenses
     else:
-        new_wallet = Wallet(total_value=total_value, total_expenses=total_expenses)
+        new_wallet = Wallet(total_value=total_value, total_expenses=total_expenses, user_id=user_id)
         db.session.add(new_wallet)
     db.session.commit()
     return redirect(url_for('dashboard'))
 
 @app.route('/add_expense', methods=['POST'])
 def add_expense():
+    if 'user_id' not in session:
+        flash('Por favor, faça login para adicionar uma despesa.', 'warning')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
     amount = request.form.get('amount')
     description = request.form.get('description')
     expense_type_id = request.form.get('expense_type_id')
-    new_expense = Expense(amount=amount, description=description, expense_type_id=expense_type_id)
+
+    new_expense = Expense(amount=amount, description=description, user_id=user_id, expense_type_id=expense_type_id)
     db.session.add(new_expense)
     db.session.commit()
     return redirect(url_for('dashboard'))
 
 @app.route('/add_revenue', methods=['POST'])
 def add_revenue():
+    if 'user_id' not in session:
+        flash('Por favor, faça login para adicionar uma despesa.', 'warning')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
+    
     amount = request.form.get('amount')
     description = request.form.get('description')
     revenue_type_id = request.form.get('revenue_type_id')
-    new_revenue = Revenue(amount=amount, description=description, revenue_type_id=revenue_type_id)
+    new_revenue = Revenue(amount=amount, description=description, revenue_type_id=revenue_type_id, user_id=user_id)
     db.session.add(new_revenue)
     db.session.commit()
     return redirect(url_for('dashboard'))
 
 @app.route('/add_expense_type', methods=['POST'])
 def add_expense_type():
+    if 'user_id' not in session:
+        flash('Por favor, faça login para adicionar uma despesa.', 'warning')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
     name = request.form.get('name')
-    new_expense_type = ExpenseType(name=name)
+    new_expense_type = ExpenseType(name=name, user_id=user_id)
     db.session.add(new_expense_type)
     db.session.commit()
     return redirect(url_for('dashboard'))
 
 @app.route('/add_revenue_type', methods=['POST'])
 def add_revenue_type():
+    if 'user_id' not in session:
+        flash('Por favor, faça login para adicionar uma despesa.', 'warning')
+        return redirect(url_for('login'))
+
+    user_id = session['user_id']
     name = request.form.get('name')
-    new_revenue_type = RevenueType(name=name)
+    new_revenue_type = RevenueType(name=name, user_id=user_id)
     db.session.add(new_revenue_type)
     db.session.commit()
     return redirect(url_for('dashboard'))
@@ -206,7 +236,8 @@ def config():
         flash('Por favor, faça login para acessar essa página.', 'warning')
         return redirect(url_for('login'))
 
-    user = User.query.get(session['user_id'])
+    user_id = session['user_id']
+    user = User.query.get(user_id)
     expense_types = ExpenseType.query.all()
     revenue_types = RevenueType.query.all()
 
@@ -215,7 +246,7 @@ def config():
         user.fullname = request.form.get('fullname')
         user.email = request.form.get('email')
         db.session.commit()
-        flash('Dados atualizados com sucesso!', 'success')
+        flash('Dados atualizados com sucesso!', 'uccess')
     
     return render_template('config.html', user=user, expense_types=expense_types, revenue_types=revenue_types)
 
