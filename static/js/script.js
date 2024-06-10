ef5326059a424d44bd4ecc738a97e92768ed6714
Ian Braga <145636380+IanBraga96@users.noreply.github.com>
2024-06-10 20:59:50 -0300

    Create script.js
    Rodapé + Separação de camadas

---
+++ static/js/script.js
@@ -0,0 +1,40 @@
+const ctx = document.getElementById('expenseRevenueChart').getContext('2d');
+const expenseRevenueChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Despesas e Receitas',
+            data: data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Tipos de Despesas e Receitas'
+            }
+        }
+    },
+});
