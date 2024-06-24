e88aaa9b8afb7ea7968b34d5782f2d395697b170
Ian Braga <145636380+IanBraga96@users.noreply.github.com>
2024-06-24 20:56:28 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
 class Config:
+    # Senha
     SECRET_KEY = 'senha_secreta_do_ian'
+
+    # URI do banco de dados, nesse caso um banco de dados SQLite chamado site.db
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+    # Desativa o rastreamento de modificações do banco de dados, melhorando o desempenho
     SQLALCHEMY_TRACK_MODIFICATIONS = False
