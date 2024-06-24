cee94c42b3886a68e8ab4c547c452dd8d74cc032
Ian Braga <145636380+IanBraga96@users.noreply.github.com>
2024-06-24 20:48:14 -0300

    Delete config.py
    A estrutura do projeto foi modificada

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'senha-secreta-rs'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
